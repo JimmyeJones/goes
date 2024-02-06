0fdd68331e83d4a3ec3860c771c9cbe76f9bd6d4
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-02-06 13:05:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.title("Quadratic Calculator")
-a = st.textbox("Enter the 'a' variable")
+a = st.text_input("Enter the 'a' variable")
 if a != int():
   st.error("Your 'a' variable is not a number")
-b = st.textbox("Enter the 'b' variable")
+b = st.text_input("Enter the 'b' variable")
 if b != int():
   st.error("Your 'b' variable is not a number")
-c = st.textbox("Enter the 'c' variable")
+c = st.text_input("Enter the 'c' variable")
 if c != int():
   st.error("Your 'c' variable is not a number")
 if st.button("Calculate"):
