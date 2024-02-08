cc53a139c9b0744ca3ee74c62e57f87e2d31c79f
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-02-08 13:41:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import math
 st.title("Quadratic Calculator")
-a = st.text_input("Enter the 'a' variable")
-b = st.text_input("Enter the 'b' variable")
-c = st.text_input("Enter the 'c' variable")
+a = int(st.text_input("Enter the 'a' variable"))
+b = int(st.text_input("Enter the 'b' variable"))
+c = int(st.text_input("Enter the 'c' variable"))
 if st.button("Calculate"):
   st.subheader((-b+math.sqrt((b*b)-4*a*c))/2*a)
