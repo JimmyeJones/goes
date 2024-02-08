827c38b9384aa12bb3f6bdb3bd0babfea0bdc841
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-02-08 10:04:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ a = st.text_input("Enter the 'a' variable")
 b = st.text_input("Enter the 'b' variable")
 c = st.text_input("Enter the 'c' variable")
 if st.button("Calculate"):
-  st.text(a)
-  st.text(b)
-  st.text(c)
+  st.subheader((-b+\sqrt{b^{2}-4ac})/2a)
