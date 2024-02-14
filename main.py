eb24fbf48f1455be124c6282c2a7e68863733b15
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-02-14 08:28:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     st.subheader("Images")
     for img_file in image_files:
         st.image(img_file, use_column_width=True)
-def create_timelapse(folder_path, image_type):
+def create_timelapse(folder_path, file_image_type):
     image_files = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith(('.jpg', '.jpeg', '.png', '.gif')) and image_type in file]
     image_files.sort(key=os.path.getmtime)
     if len(image_files) == 0:
