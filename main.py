2e27eabb01f90d4dbd23a7b9838f9638dc56ec02
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-02-15 11:24:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def main():
     output_type = st.sidebar.selectbox("Select a display type", ["Timelape", "Images"])
     date_day = st.sidebar.text_input("Enter the day you want to view (ONLY NUMBER)")
     date_month = st.sidebar.text_input("Enter the month you want to view (ONLY NUMBER)")
+    date_year = st.sidebar.text_input("Enter the year you want to view (ONLY NUMBER)")
     
     
     image_files = [os.path.join(folder_selected, file) for file in os.listdir(folder_selected) if file.endswith(('.jpg', '.jpeg', '.png', '.gif')) and file_image_type in file]
